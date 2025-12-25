04f34c0fdd7633fce6aa129fd63ee9890fd0f944
PanYao <984080885@qq.com>
2025-12-25 16:48:11 +0800

    Initial commit: QT project with .gitignore

---
+++ mainwindow.h
@@ -0,0 +1,33 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+/**********************************************************************************************************************************
+ *
+ * Project Title:ChatRoom
+ *
+ * Author:PanYao Nju
+ *
+ * Brief:一次关于C/C++技术栈的应用实践
+ *
+ * ********************************************************************************************************************************/
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
